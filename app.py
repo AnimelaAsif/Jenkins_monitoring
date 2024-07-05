eb340ee7adae92b12119d35715c62656809b5328
AnimelaAsif <110332127+AnimelaAsif@users.noreply.github.com>
2024-07-05 17:25:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import requests
-
+import json
 JENKINS_URL = 'http://13.233.29.101:8080'
 USERNAME = 'mohammedasif'
 PASSWORD = 'Rmn@73383271'
@@ -11,12 +11,11 @@ try:
     response = requests.get(api_url, auth=auth)
     if response.status_code == 200:
         build_details = response.json()
-        build_id = build_details['id']
+        build_id = build_details['id']    
         print(f"Build ID of build number {BUILD_NUMBER} in pipeline {PIPELINE_NAME} is: {build_id}")
-        
         with open('api.log', 'w') as f:
-            f.write(response.text)     
-        print("Response content written to api.log file.")
+            json.dump(build_details, f, indent=4)
+        print("Pretty-printed JSON content written to api.log file.")
     else:
         print(f"Failed to retrieve build details. Status code: {response.status_code}")
 except requests.exceptions.RequestException as e:
