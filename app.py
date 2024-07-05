feb9763ada445897cd0cf3e6e7643e575298b7a8
AnimelaAsif <110332127+AnimelaAsif@users.noreply.github.com>
2024-07-05 17:21:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import requests
 
 JENKINS_URL = 'http://13.233.29.101:8080'
 USERNAME = 'mohammedasif'
-PASSWORD = 'Rmn@12345'
+PASSWORD = 'Rmn@73383271'
 PIPELINE_NAME = 'monitoring'
 BUILD_NUMBER = '7'
 api_url = f"{JENKINS_URL}/job/{PIPELINE_NAME}/{BUILD_NUMBER}/api/json"
