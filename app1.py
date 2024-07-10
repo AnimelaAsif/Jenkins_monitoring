b4dc288ba17731bf541dba9c8700b9b4403e3ceb
AnimelaAsif <110332127+AnimelaAsif@users.noreply.github.com>
2024-07-10 12:03:04 +0530

    Create app1.py

---
+++ app1.py
@@ -0,0 +1,28 @@
+import requests
+
+def fetch_console_output(job_name, build_number):
+    base_url = 'http://65.2.74.65:8080/'
+    api_url = f'{base_url}job/{job_name}/{build_number}/consoleText'
+
+    try:
+        response = requests.get(api_url, auth=('your_username', 'your_api_token_or_password'))
+
+        if response.status_code == 200:
+            return response.text
+        else:
+            print(f"Failed to fetch console output. Status code: {response.status_code}")
+            return None
+
+    except requests.exceptions.RequestException as e:
+        print(f"Failed to fetch console output: {str(e)}")
+        return None
+
+# Example usage:
+if __name__ == "__main__":
+    job_name = 'monitoring'
+    build_number = '1'
+
+    console_output = fetch_console_output(job_name, build_number)
+    if console_output:
+        print(f"Console output for job '{job_name}' build #{build_number}:")
+        print(console_output)
